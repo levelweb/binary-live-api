b4a81e988bbc7cf534bd6ca52bcaeb30321d6ec5
Артем Ковалев <tema.level@gmail.com>
2021-03-23 15:46:15 +0700

    df

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,30 @@
-var gulp        = require('gulp');
-var gh          = require('gulp-gh-pages');
-var webpack     = require('webpack');
-var gutil       = require('gulp-util');
-var pkg         = require('./package.json');
+var gulp = require('gulp');
+var gh = require('gulp-gh-pages');
+var webpack = require('webpack');
+var gutil = require('gulp-util');
+var pkg = require('./package.json');
 /**
  * Push build to gh-pages
  */
 
-gulp.task('build', function(callback) {
-    webpack(require('./webpack.config.js'), function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString());
+gulp.task('build', (callback) => {
+    webpack(require('./webpack.config.js'), (err, stats) => {
+        if (err) throw new gutil.PluginError('webpack', err);
+        gutil.log('[webpack]', stats.toString());
 
         callback();
     });
 });
 
-gulp.task('versioning', ['build'], function () {
+gulp.task('vers', () => {
     var v = pkg.version;
-    return gulp.src(['lib/*.*'])
-        .pipe(gulp.dest('lib/' + v));
+    return gulp.src(['lib/*.*']).pipe(gulp.dest(`lib/${  v}`));
 });
+gulp.task('versioning', gulp.series('build', 'vers'));
 
-gulp.task('deploy', ['versioning'], function () {
-    return gulp.src(["./lib/**/*", "./CNAME"])
-        .pipe(gh({ force: true }));
-});
+gulp.task('depl', () => gulp.src(['./lib/**/*']).pipe(gh({ force: true })));
+gulp.task('deploy', gulp.series('versioning', 'depl'));
 
-gulp.task('deploy-prod', ['versioning'], function () {
-    return gulp.src(["./lib/**/*", "./CNAME"])
-        .pipe(gh({ force: true, origin: 'upstream' }));
-});
+gulp.task('depl-prod', () => gulp.src(['./lib/**/*']).pipe(gh({ force: true, origin: 'upstream' })));
+
+gulp.task('deploy-prod', gulp.series('versioning', 'depl-prod'));
